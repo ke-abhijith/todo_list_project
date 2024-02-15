6ad2b2077141a5b760dfd195b263f7f09ff746be
ke-abhijith <abhijithmuttar7@gmail.com>
2024-02-15 14:41:24 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import './App.css'
+
+import Todo from './components/Todo'
+function App() {
+
+
+  return (
+
+    
+
+    <>
+     
+     
+    
+      <Todo/>
+
+
+      </>
+     
+    
+    
+  )
+}
+
+export default App
